d410d8eb7f30a7322584ce1f4657c72927ae222a
bigrocs <bigrocs@qq.com>
2019-08-10 07:57:41 +0800

    自动提交 git 代码

--- main_test.go
+++ main_test.go
@@ -12,7 +12,7 @@ func TestAddGoods(t *testing.T) {
 		BaseHost: "http://webapi.chinatrace.org",
 		Key:      "V7N3Xpm4jpRon/WsZ8X/63G8oMeGdUkA8Luxs1CenTY=",
 	}
-	data, err := chinatrace.Get("6917878036526")
+	data, err := chinatrace.Get("6920373400006")
 	fmt.Println(data, err)
 	t.Log(t)
 }
