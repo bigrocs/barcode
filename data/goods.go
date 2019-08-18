bf5477e27f4a8d54d1c17bbcbead16ec6151836f
bigrocs <bigrocs@qq.com>
2019-08-18 11:04:02 +0800

    自动提交 git 代码

--- data/goods.go
+++ data/goods.go
@@ -5,15 +5,15 @@ type Goods struct {
 	Barcode       string   // 条码
 	Name          string   // 产品名称
 	EnName        string   // 产品英文名称
-	Images         []string // 产品图片
+	Images        []string // 产品图片
 	BrandName     string   // 品牌
 	Specification string   // 规格
 	Unit          string   // 单位
 	Width         int64    // 宽(毫米)
 	Height        int64    // 高(毫米)
 	Depth         int64    // 深(毫米)
-	NetWeight     int64    // 净重(克)
-	GrossWeight   int64    // 总重(克)
+	Netweight     int64    // 净重(克)
+	Grossweight   int64    // 总重(克)
 	Unspsc        int64    // 商品及服务编码
 	UnspscName    string   // 商品及服务编码分类名称
 	Source        string   // 产地代码
